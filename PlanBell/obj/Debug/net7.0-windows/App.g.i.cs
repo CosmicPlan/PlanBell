d6c66fe6634184695586aec97994e1312a5f15f3
XST <173324094@qq.com>
2023-12-07 18:36:22 +0800

    更新

--- PlanBell/obj/Debug/net7.0-windows/App.g.i.cs
+++ PlanBell/obj/Debug/net7.0-windows/App.g.i.cs
@@ -77,7 +77,7 @@ namespace PlanBell {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/PlanBell;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PlanBell;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
